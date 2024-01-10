9714f7c0149d07fe8e5337f47320d5e67925824a
vision800 <netshokotshokovision112@gmail.com>
2024-01-10 10:22:29 +0200

    Add project files.

---
+++ Movie.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21__Getters_and_setters
+{
+    internal class Movie
+    {
+        public string title;
+        public string diractor;
+        public string rating;
+
+        public Movie(string title, string diractor, string rating)
+        {
+            this.title = title;
+            this.diractor = diractor;
+            this.rating = rating;
+        }
+
+        public string Rating 
+        {
+            get{ return rating; }
+            set{
+            if( value=="G"|| value == "PG" || value == "PG-13" || value == "R" ) rating = value;
+            else rating = "NR";
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21__Getters_and_setters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie movie = new Movie("Titanic","vision","PG-13");
+            Movie movie1 = new Movie("Catching fire", "vision", "PG");
+
+            Console.WriteLine(movie1.rating);
+            Console.Read();
+        }
+    }
+}
